28a3065c7a7ce155cde71d4e8f49e5f4ddbaeaf3
Ricardo Miranda Zuniga <rmira@hunter.cuny.edu>
2025-04-22 20:22:23 -0400

    Update RotateSky.cs

--- RotateSky.cs
+++ RotateSky.cs
@@ -9,7 +9,8 @@ public class RotateSky : MonoBehaviour
     {
       //https://docs.unity3d.com/ScriptReference/RenderSettings.html
       /*
-      It is usually used as a prefix for private variable names as a visual indication that the scope of the variable is private to the class in which it is declared. It thereby increases readability.
+      It is usually used as a prefix for private variable names as a visual indication that the scope
+      of the variable is private to the class in which it is declared. It thereby increases readability.
       */
       RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
     }
